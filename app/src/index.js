433e682bfcabd7c907bb747d70f0164e2b2fcffd
r0994995 <r0994995@ucll.be>
2025-12-09 10:02:15 +0100

    second commit

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, from my CI/CD & GitOps app');
+    res.send('Hello from eindwerk-cc from CI/CD & GitOps app');
 });
 
 app.listen(3000, () => console.log('App running on port 3000'));
